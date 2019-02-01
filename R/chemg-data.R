30767bc363cae01126f1650621c0ee30a2c8fe82
Ted Hsuan Yun Chen <ted.hsuanyun.chen@gmail.com>
2019-02-01 05:31:41 -0500

    added chemg data and documentation

---
+++ R/chemg-data.R
@@ -0,0 +1,95 @@
+#' ChemG Committees Network
+#'
+#' An undirected network of all 30 ChemG actors where the weighted ties are number of shared committees.
+#'
+#' @docType data
+#'
+#' @usage data(committee)
+#'
+#' @format A matrix with 30 columns and 30 rows.
+#'
+#' @references 
+#' Replication materials for Leifeld, P and V Schneider, Information Exchange in Policy Networks. American Journal of Political Science 53(3): 731-744. \url{http://dx.doi.org/10.1111/j.1540-5907.2011.00580.x}
+#'
+#' @source Dataverse: \url{http://hdl.handle.net/1902.1/17004}
+"committee"
+
+#' ChemG Influence Network
+#'
+#' A directed network of all 30 ChemG actors where the binary directed ties indicate the row actor cited the column actor as influential.
+#'
+#' @docType data
+#'
+#' @usage data(infrep)
+#'
+#' @format A matrix with 30 columns and 30 rows.
+#'
+#' @references 
+#' Replication materials for Leifeld, P and V Schneider, Information Exchange in Policy Networks. American Journal of Political Science 53(3): 731-744. \url{http://dx.doi.org/10.1111/j.1540-5907.2011.00580.x}
+#'
+#' @source Dataverse: \url{http://hdl.handle.net/1902.1/17004}
+"infrep"
+
+#' ChemG Political Information Network
+#'
+#' A directed network of all 30 ChemG actors where the binary directed ties indicate the row actor cited the column actor as one it exchanges political/strategic information with.
+#'
+#' @docType data
+#'
+#' @usage data(pol)
+#'
+#' @format A matrix with 30 columns and 30 rows.
+#'
+#' @references 
+#' Replication materials for Leifeld, P and V Schneider, Information Exchange in Policy Networks. American Journal of Political Science 53(3): 731-744. \url{http://dx.doi.org/10.1111/j.1540-5907.2011.00580.x}
+#'
+#' @source Dataverse: \url{http://hdl.handle.net/1902.1/17004}
+"pol"
+
+#' ChemG Preference Similarity Network
+#'
+#' An undirected network of all 30 ChemG actors where the weighted ties indicate preference similarity between two actors on six core policy topics.
+#'
+#' @docType data
+#'
+#' @usage data(prefsim)
+#'
+#' @format A matrix with 30 columns and 30 rows.
+#'
+#' @references 
+#' Replication materials for Leifeld, P and V Schneider, Information Exchange in Policy Networks. American Journal of Political Science 53(3): 731-744. \url{http://dx.doi.org/10.1111/j.1540-5907.2011.00580.x}
+#'
+#' @source Dataverse: \url{http://hdl.handle.net/1902.1/17004}
+"prefsim"
+
+#' ChemG Scientific Information Network
+#'
+#' A directed network of all 30 ChemG actors where the binary directed ties indicate the row actor cited the column actor as one it sends scientific/technical information to.
+#'
+#' @docType data
+#'
+#' @usage data(sci)
+#'
+#' @format A matrix with 30 columns and 30 rows.
+#'
+#' @references 
+#' Replication materials for Leifeld, P and V Schneider, Information Exchange in Policy Networks. American Journal of Political Science 53(3): 731-744. \url{http://dx.doi.org/10.1111/j.1540-5907.2011.00580.x}
+#'
+#' @source Dataverse: \url{http://hdl.handle.net/1902.1/17004}
+"sci"
+
+#' ChemG Actor Types
+#'
+#' One of five organization types for each of the 30 ChemG actors. The types of organizations are government agencies, interest groups, international organizations, political parties, and scientific organizations. 
+#'
+#' @docType data
+#'
+#' @usage data(types)
+#'
+#' @format A character vector of length 30.
+#'
+#' @references 
+#' Replication materials for Leifeld, P and V Schneider, Information Exchange in Policy Networks. American Journal of Political Science 53(3): 731-744. \url{http://dx.doi.org/10.1111/j.1540-5907.2011.00580.x}
+#'
+#' @source Dataverse: \url{http://hdl.handle.net/1902.1/17004}
+"types"
