75378bd70786543a7a0c58a47d90b12d7e1a559c
Ted Hsuan Yun Chen <ted.hsuanyun.chen@gmail.com>
2019-02-13 17:37:37 -0500

    added package documentation

---
+++ R/multilayer.ergm.R
@@ -0,0 +1,7 @@
+#' multilayer.ergm: exponential random graph models for multilayer networks
+#' 
+#' This package extends exponential random graph models to applications with multilayer networks.
+#' 
+#' @useDynLib NetworkInference, .registration = TRUE
+#' @name multilayer.ergm
+NULL
