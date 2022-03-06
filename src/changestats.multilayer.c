b5f11e17add8f153239c2318cb7b69d30dd53eea
Ted Hsuan Yun Chen <ted.hsuanyun.chen@gmail.com>
2022-03-06 19:34:22 +0200

    removed assignment of layermem into separate var for gwtesp

--- src/changestats.multilayer.c
+++ src/changestats.multilayer.c
@@ -790,7 +790,7 @@ D_CHANGESTAT_FN(d_gwesp_layer) {
 D_CHANGESTAT_FN(d_gwtesp_layer) { 
 	Edge e, f;
 	int i, echange, ochange;
-	int L2th, L2tu, L2uh, l, layer_mem[N_NODES];
+	int L2th, L2tu, L2uh, l;
 	Vertex tail, head, u, v;
 	double alpha, oneexpa, cumchange;
   
@@ -798,25 +798,22 @@ D_CHANGESTAT_FN(d_gwtesp_layer) {
 	alpha = INPUT_PARAM[0];
 	oneexpa = 1.0-exp(-alpha);
 	l = INPUT_PARAM[1];
-	for(i = 0; i < N_NODES; i++){
-		layer_mem[i] = INPUT_PARAM[2 + i];
-	}
 	
 	/* *** don't forget tail -> head */    
 	FOR_EACH_TOGGLE(i){
 		tail = TAIL(i); head = HEAD(i);
-		if(layer_mem[tail - 1] == l && layer_mem[head - 1] == l){
+		if(INPUT_PARAM[tail + 1] == l && INPUT_PARAM[head + 1] == l){
 			cumchange=0.0;
 			L2th=0;
 			ochange = IS_OUTEDGE(tail, head) ? -1 : 0;
 			echange = 2*ochange + 1;
 			/* step through outedges of head  */
 			STEP_THROUGH_OUTEDGES(head, e, u){
-				if (IS_OUTEDGE(tail, u) && layer_mem[u - 1] == l){
+				if (IS_OUTEDGE(tail, u) && INPUT_PARAM[u + 1] == l){
 					L2tu=ochange;
 					/* step through inedges of u */
 					STEP_THROUGH_INEDGES(u, f, v){
-						if(IS_OUTEDGE(tail, v) && layer_mem[v - 1] == l) L2tu++;
+						if(IS_OUTEDGE(tail, v) && INPUT_PARAM[v + 1] == l) L2tu++;
 					}
 					cumchange += pow(oneexpa,(double)L2tu);
 				}
@@ -827,11 +824,11 @@ D_CHANGESTAT_FN(d_gwtesp_layer) {
 				if (IS_OUTEDGE(tail, u)){
 					L2th++;
 				}
-				if (IS_OUTEDGE(u, tail) && layer_mem[u - 1] == l){
+				if (IS_OUTEDGE(u, tail) && INPUT_PARAM[u + 1] == l){
 					L2uh=ochange;
 					/* step through outedges of u */
 					STEP_THROUGH_OUTEDGES(u, f, v){
-						if(IS_OUTEDGE(v, head) && layer_mem[v - 1] == l) L2uh++;
+						if(IS_OUTEDGE(v, head) && INPUT_PARAM[v + 1] == l) L2uh++;
 					}
 					cumchange += pow(oneexpa,(double)L2uh) ;
 				}
